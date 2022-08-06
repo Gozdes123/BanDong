f5209b73d17a3594fd6ba54098ed8ea33a7a8b1b
shesid2698 <shesid2698@gmail.com>
2022-08-06 17:33:06 +0800

    新增訂購系統_訂購功能

--- Order_Form.cs
+++ Order_Form.cs
@@ -13,7 +13,7 @@ namespace BanDong_1._0v
 {
     public partial class Order_Form : Form
     {
-        DataSet ds=new DataSet();
+        DataSet ds = new DataSet();
         public Order_Form()
         {
             InitializeComponent();
@@ -21,21 +21,39 @@ namespace BanDong_1._0v
 
         private void label2_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void BTN_Buy_Click_1(object sender, EventArgs e)
         {
-            
+
             using (SqlConnection cn = new SqlConnection(Login_Form.sqlcn))
             {
                 cn.Open();
-                string insert = $"insert into Orders(Ticket_Money, StudentName, Type, Remark, OrderDateTime) Values('{cb_PayType.Text}', '{labelName.Text}', '{cb_BanDongType.Text}', '{tbox_remark.Text}', '{DateTime.Now.ToString()}');";
-                SqlDataAdapter da_buy=new SqlDataAdapter(insert, cn);
-                da_buy.Fill(ds,"BuyBanDong");
-                DataTable dt = ds.Tables[0];
+                if (cb_PayType.Text != "" && cb_BanDongType.Text != "")
+                {
+                    string insert = $"insert into Orders(Ticket_Money, StudentName, Type, Remark, OrderDateTime) Values('{cb_PayType.Text}', '{labelName.Text}', '{cb_BanDongType.Text}', '{tbox_remark.Text}', '{DateTime.Now.ToString()}');";
+                    SqlDataAdapter da_buy = new SqlDataAdapter(insert, cn);
+                    da_buy.Fill(ds, "BuyBanDong");
+                    BTN_Buy.Enabled = false;
+                    MessageBox.Show("訂購成功!!");
+                }
+                else if (cb_PayType.Text == "" && cb_BanDongType.Text != "")
+                {
+                    MessageBox.Show("付款方式請勿空白");
+                    BTN_Buy.Enabled = true;
+
+                }
+                else if (cb_PayType.Text != "" && cb_BanDongType.Text == "")
+                {
+                    MessageBox.Show("便當類型請勿空白");
+                    BTN_Buy.Enabled = true;
+                }
+                else MessageBox.Show("欄位請勿空白!");
+               
+
             }
-            //"insert into Orders(Ticket_Money, StudentName, Type, Remark, OrderDateTime) Values('票券', '小白', '圓形', '素食', '2022-08-05 15:59');"
+
 
         }
     }
